6591de0c11beefa5ef402f60b4180f5ac3e6411f
Googol Lee <i@googol.im>
2024-11-28 10:46:16 +0100

    Update comments.

--- config.go
+++ config.go
@@ -2,17 +2,16 @@
 package clic is short for CLI Config. It implements a framework to load/parse cli configuration from a file, environment or flags.
 
 Usage:
-
-  - Register functions ([RegisterWithCallback] and [RegisterAndGet]) should be called in `init()` of a package, or before [Init] calls.
-  - [Init] function should be called at the beginning of `main()`, before calling functions in other sub-packages.
-  - [Init] function must not be called in `init()`, because other sub-packages may not be initialized at that time.
+  - Register functions ([RegisterWithCallback] and [RegisterAndGet]) should be called in "init()" of a package, or before [Init] calls.
+  - [Init] function should be called at the beginning of "main()", before calling functions in other sub-packages.
+  - [Init] function must not be called in "init()", because other sub-packages may not be initialized at that time.
 */
 package clic
 
 import "context"
 
 /*
-RegisterAndGet registers a `Config` struct with the `name` as the scope name and returns a function `getter` to get the `Config` instance from the context. [Init] function parses configuration from a file, environment or flags, stores a `Config` instance into the returned context.
+RegisterAndGet registers a "Config" struct with the "name" as the scope name and returns a function "getter" to get the "Config" instance from the context. [Init] function parses configuration from a file, environment or flags, stores a "Config" instance into the returned context.
 
 Example:
 
@@ -36,7 +35,7 @@ func RegisterAndGet[Config any](name string) (getter func(ctx context.Context) *
 }
 
 /*
-RegisterWithCallback registers a  `Config` struct with the `name` as the scope name and `callback` function to consume the parsed instance of `Config`. [Init] function parses configuration from a file, environment or flags, stores into an instance of `Config`, then call `callback` with that instance. Then `callback` function could initialize global instances. If `callback` returns an error, [Init] fails and returns a wrapped error.
+RegisterWithCallback registers a  "Config" struct with the "name" as the scope name and "callback" function to consume the parsed instance of "Config". [Init] function parses configuration from a file, environment or flags, stores into an instance of "Config", then call "callback" with that instance. Then "callback" function could initialize global instances. If "callback" returns an error, [Init] fails and returns a wrapped error.
 
 Example:
 
@@ -45,6 +44,20 @@ Example:
 	import (
 	  // ...
 	)
+
+	func main() {
+	  ctx := context.Background()
+	  clic.Init(ctx)
+
+	  slog.Info("Hello, clic!")
+	}
+
+  // To improve code organization and maintainability, consider moving the following code into a separate package. 
+  // If you do this, remember to import the new package into the "main" package to ensure proper functionality.
+
+	func init() {
+	  clic.RegisterWithCallback(initLogger)
+	}
 
 	type logConfig struct {
 	  Level string `clic:"level,info,the minimum level of logging: <debug|info|warn|error>"`
@@ -58,7 +71,7 @@ Example:
 	    slog.SetLogLoggerLevel(slog.LevelInfo)
 	  case "warn":
 	    slog.SetLogLoggerLevel(slog.LevelWarn)
-	  case "Error":
+	  case "error":
 	    slog.SetLogLoggerLevel(slog.LevelError)
 	  default:
 	    return fmt.Errorf("invalid log level: %q", cfg.Level)
@@ -66,23 +79,12 @@ Example:
 
 	  return nil
 	}
-
-	func init() {
-	  clic.RegisterWithCallback(initLogger)
-	}
-
-	func main() {
-	  ctx := context.Background()
-	  clic.Init(ctx)
-
-	  slog.Info("Hello, clic!")
-	}
 */
 func RegisterWithCallback[Config any](name string, callback func(ctx context.Context, cfg *Config) error) {
 }
 
 // Init parses configuration from a file, environment or flags.
 //
-// If any error happens during calling, `Init()` prints that error on Stderr and calls [os.Exit(125)] to exit.
+// If any error happens during calling, "Init()" prints that error on Stderr and calls [os.Exit(125)] to exit.
 func Init(ctx context.Context) {
 }
