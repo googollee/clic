e7e97b5a3e6c2fc6408b76fac74e30afdfe38e46
Googol Lee <i@googol.im>
2024-11-28 10:50:05 +0100

    Update comments.

--- config.go
+++ config.go
@@ -15,20 +15,20 @@ RegisterAndGet registers a "Config" struct with the "name" as the scope name and
 
 Example:
 
-		package main
+	package main
 
-		import (
-			// ...
-		)
+	import (
+		// ...
+	)
 
-		var dbConfig = clic.RegisterAndGet[database.Config]("database")
+	var dbConfig = clic.RegisterAndGet[database.Config]("database")
 
-		func main() {
-	    ctx := context.Background()
-			clic.Init(ctx)
+	func main() {
+		ctx := context.Background()
+		clic.Init(ctx)
 
-			db := database.New(dbConfig(ctx))
-		}
+		db := database.New(dbConfig(ctx))
+	}
 */
 func RegisterAndGet[Config any](name string) (getter func(ctx context.Context) *Config) {
 	return
@@ -42,42 +42,42 @@ Example:
 	package main
 
 	import (
-	  // ...
+		// ...
 	)
 
 	func main() {
-	  ctx := context.Background()
-	  clic.Init(ctx)
+		ctx := context.Background()
+		clic.Init(ctx)
 
-	  slog.Info("Hello, clic!")
+		slog.Info("Hello, clic!")
 	}
 
-  // To improve code organization and maintainability, consider moving the following code into a separate package. 
-  // If you do this, remember to import the new package into the "main" package to ensure proper functionality.
+	// To improve code organization and maintainability, consider moving the following code into a separate package.
+	// If you do this, remember to import the new package into the "main" package to ensure proper functionality.
 
 	func init() {
-	  clic.RegisterWithCallback(initLogger)
+		clic.RegisterWithCallback(initLogger)
 	}
 
 	type logConfig struct {
-	  Level string `clic:"level,info,the minimum level of logging: <debug|info|warn|error>"`
+		Level string `clic:"level,info,the minimum level of logging: <debug|info|warn|error>"`
 	}
 
 	func initLogger(ctx context.Context, cfg *logConfig) error {
-	  switch cfg.Level {
-	  case "debug":
-	    slog.SetLogLoggerLevel(slog.LevelDebug)
-	  case "info":
-	    slog.SetLogLoggerLevel(slog.LevelInfo)
-	  case "warn":
-	    slog.SetLogLoggerLevel(slog.LevelWarn)
-	  case "error":
-	    slog.SetLogLoggerLevel(slog.LevelError)
-	  default:
-	    return fmt.Errorf("invalid log level: %q", cfg.Level)
-	  }
+		switch cfg.Level {
+		case "debug":
+			slog.SetLogLoggerLevel(slog.LevelDebug)
+		case "info":
+			slog.SetLogLoggerLevel(slog.LevelInfo)
+		case "warn":
+			slog.SetLogLoggerLevel(slog.LevelWarn)
+		case "error":
+			slog.SetLogLoggerLevel(slog.LevelError)
+		default:
+			return fmt.Errorf("invalid log level: %q", cfg.Level)
+		}
 
-	  return nil
+		return nil
 	}
 */
 func RegisterWithCallback[Config any](name string, callback func(ctx context.Context, cfg *Config) error) {
@@ -85,6 +85,8 @@ func RegisterWithCallback[Config any](name string, callback func(ctx context.Con
 
 // Init parses configuration from a file, environment or flags.
 //
-// If any error happens during calling, "Init()" prints that error on Stderr and calls [os.Exit(125)] to exit.
+// If any error happens during calling, "Init()" prints that error on Stderr and calls "[os.Exit(125)]" to exit.
+//
+// [os.Exist(125)]: https://pkg.go.dev/os#Exit
 func Init(ctx context.Context) {
 }
